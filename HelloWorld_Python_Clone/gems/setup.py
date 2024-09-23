7dc2ea6653e8ad02737982948e688362abc0c21d
yuvraj@prophecy.io <yuvraj@prophecy.io>
2024-09-23 10:22:22 +0000

    [Prophecy] Release version v3

--- HelloWorld_Python_Clone/gems/setup.py
+++ HelloWorld_Python_Clone/gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'abhishekse2etestsprophecyioteam_helloworld',
-    version = '2',
+    version = '3',
     packages = packages_to_include,
     description = '',
     install_requires = [],
